bd1e277b4e648e3e15c78eba4a944c65b41a1a23
Multiloginapp <reg@multiloginapp.com>
2016-06-22 16:07:23 +0200

    v1.0.7
    Fixed security issues reported by Niklas Gollenstede

--- data/js/content.js
+++ data/js/content.js
@@ -1,127 +1,142 @@
-function main(r, g, b, a) {
-    //console.log("r" + r + "g" + g + "b" + b + "a" + a);
-    function override(rootElem, functName) {
-        //console.log("overriding " + functName);
-        var old = rootElem[functName];
-        rootElem[functName] = function (arg1, arg2) {
-            //console.log("call overridden " + functName);
-            var element = old.apply(this, arguments);
-            if (element == null) {
-                return null;
-            }
-            if (Object.prototype.toString.call(element) === '[object HTMLCollection]' ||
-                Object.prototype.toString.call(element) === '[object NodeList]') {
-                for (var i = 0; i < element.length; ++i) {
-                    var el = element[i];
-                    //console.log("elements list inject " + functName);
-                    inject(el);
-                }
-            } else {
-                //console.log("element inject " + functName);
-                inject(element);
-            }
-
-            return element;
+var allowInjection = true;
+if (window.frameElement != null && window.frameElement.sandbox != null) {
+    allowInjection = false;
+    for (var i = 0; i < window.frameElement.sandbox.length; i++) {
+        var val = window.frameElement.sandbox[i];
+        if (val == 'allow-scripts') {
+            allowInjection = true;
         }
     }
-
+}
+function main(r, g, b, a, scriptId) {
+    var scriptNode = document.getElementById(scriptId);
+    var canvasElems = {};
+    function overrideCanvasProto(root) {
+        var orig = root.prototype.getContext;
+        Object.defineProperty(root.prototype, "getContext",
+            {
+                value: function () {
+                    var res = orig.apply(this, arguments);
+                    canvasElems[res] = this;
+                    return res;
+                }
+            }
+        );
+    }
+    function overrideCanvaRendProto(root) {
+        var getImageData = root.prototype.getImageData;
+        var putImageData = root.prototype.putImageData;
+        function overrideProtoFunct(old, name) {
+            Object.defineProperty(root.prototype, name,
+                {
+                    value: function () {
+                        //console.log(name + "call");
+                        old.apply(this, arguments);
+                        var canvasElem = canvasElems[this];
+                        var width = 300;
+                        var height = 150;
+                        if (canvasElem) {
+                            width = canvasElem.width;
+                            height = canvasElem.height;
+                        } else {
+                            console.log("canvas size is unknown");
+                        }
+                        var imageData = getImageData.call(this, 0, 0, width, height);
+                        for (var i = 0; i < height; i++) {
+                            for (var j = 0; j < width; j++) {
+                                var index = ((i * (width * 4)) + (j * 4));
+                                imageData.data[index + 0] = imageData.data[index + 0] + r;
+                                imageData.data[index + 1] = imageData.data[index + 1] + g;
+                                imageData.data[index + 2] = imageData.data[index + 2] + b;
+                                imageData.data[index + 3] = imageData.data[index + 3] + a;
+                            }
+                        }
+                        putImageData.call(this, imageData, 0, 0);
+                    }
+                }
+            );
+        }
+        overrideProtoFunct(root.prototype.clearRect, "clearRect");
+        overrideProtoFunct(root.prototype.fillRect, "fillRect");
+        overrideProtoFunct(root.prototype.strokeRect, "strokeRect");
+        //Drawing text
+        overrideProtoFunct(root.prototype.fillText, "fillText");
+        overrideProtoFunct(root.prototype.strokeText, "strokeText");
+        //TODO
+        //   overrideProtoFunct(root.prototype.measureText, "measureText");
+        //Drawing paths
+        overrideProtoFunct(root.prototype.fill, "fill");
+        //overrideProtoFunct(root.prototype.stroke, "stroke");
+        overrideProtoFunct(root.prototype.drawFocusIfNeeded, "drawFocusIfNeeded");
+        overrideProtoFunct(root.prototype.clip, "clip");
+        overrideProtoFunct(root.prototype.isPointInPath, "isPointInPath");
+        overrideProtoFunct(root.prototype.isPointInStroke, "isPointInStroke");
+        //Drawing images
+        overrideProtoFunct(root.prototype.drawImage, "drawImage");
+        overrideProtoFunct(root.prototype.putImageData, "putImageData");
+        overrideProtoFunct(root.prototype.getImageData, "getImageData");
+    }
     function inject(element) {
         if (element.tagName.toUpperCase() === "IFRAME") {
             try {
-                if (element.contentDocument == null) {
-                    //console.log("iframe contentDocument is null");
-                    return;
-                }
-                override(element.contentDocument, "createElement");
-                override(element.contentDocument, "createElementNS");
-                override(element.contentDocument, "getElementById");
-                override(element.contentDocument, "getElementsByName");
-                override(element.contentDocument, "getElementsByClassName");
-                override(element.contentDocument, "getElementsByTagName");
-                override(element.contentDocument, "getElementsByTagNameNS");
+                overrideCanvasProto(element.contentWindow.HTMLCanvasElement);
+                overrideCanvaRendProto(element.contentWindow.CanvasRenderingContext2D);
+                overrideDocumentProto(element.contentWindow.Document);
             } catch (e) {
-                //console.log(e);
+                console.error(e);
             }
-        } else if (element.tagName.toUpperCase() === "CANVAS") {
-            var ctx = element.getContext("2d");
-            injectCanvas(ctx, element.height, element.width);
         }
     }
-
-    function injectCanvas(ctx, height, width) {
-        //console.log("canvas  h:" + height + " w:" + width);
-        //console.log("inject canvas");
-
-        var origGetImageData = ctx.getImageData;
-        var origPutImageData = ctx.putImageData;
-
-        function overrideCanvas(ctx, functName) {
-            var old = ctx[functName];
-            ctx[functName] = function () {
-                //console.log("call canvas overridden " + functName);
-                old.apply(this, arguments);
-
-                var imageData = origGetImageData.call(ctx, 0, 0, width, height);
-                for (var i = 0; i < height; i++) {
-                    for (var j = 0; j < width; j++) {
-                        var index = ((i * (width * 4)) + (j * 4));
-                        imageData.data[index + 0] = imageData.data[index + 0] + r;
-                        imageData.data[index + 1] = imageData.data[index + 1] + g;
-                        imageData.data[index + 2] = imageData.data[index + 2] + b;
-                        imageData.data[index + 3] = imageData.data[index + 3] + a;
+    function overrideDocumentProto(root) {
+        function doOverrideDocumentProto(old, name) {
+            Object.defineProperty(root.prototype, name,
+                {
+                    value: function () {
+                        var element = old.apply(this, arguments);
+                        //console.log(name+ " everridden call"+element);
+                        if (element == null) {
+                            return null;
+                        }
+                        if (Object.prototype.toString.call(element) === '[object HTMLCollection]' ||
+                            Object.prototype.toString.call(element) === '[object NodeList]') {
+                            for (var i = 0; i < element.length; ++i) {
+                                var el = element[i];
+                                //console.log("elements list inject " + name);
+                                inject(el);
+                            }
+                        } else {
+                            //console.log("element inject " + name);
+                            inject(element);
+                        }
+                        return element;
                     }
                 }
-                origPutImageData.call(ctx, imageData, 0, 0);
-            }
+            );
         }
-
-        // Drawing rectangles
-        overrideCanvas(ctx, "clearRect");
-        overrideCanvas(ctx, "fillRect");
-        overrideCanvas(ctx, "strokeRect");
-        //Drawing text
-        overrideCanvas(ctx, "fillText");
-        overrideCanvas(ctx, "strokeText");
-
-        //Drawing paths
-        overrideCanvas(ctx, "fill");
-        //overrideCanvas(ctx, "stroke");
-        overrideCanvas(ctx, "drawFocusIfNeeded");
-        overrideCanvas(ctx, "scrollPathIntoView");
-        overrideCanvas(ctx, "clip");
-        overrideCanvas(ctx, "isPointInPath");
-        overrideCanvas(ctx, "isPointInStroke");
-
-        //Drawing images
-        overrideCanvas(ctx, "drawImage");
-
-        overrideCanvas(ctx, "putImageData");
-        overrideCanvas(ctx, "getImageData");
+        doOverrideDocumentProto(root.prototype.createElement, "createElement");
+        doOverrideDocumentProto(root.prototype.createElementNS, "createElementNS");
+        doOverrideDocumentProto(root.prototype.getElementById, "getElementById");
+        doOverrideDocumentProto(root.prototype.getElementsByClassName, "getElementsByClassName");
+        doOverrideDocumentProto(root.prototype.getElementsByTagName, "getElementsByTagName");
+        doOverrideDocumentProto(root.prototype.getElementsByTagNameNS, "getElementsByTagNameNS");
     }
-
-    override(document, "createElement");
-    override(document, "createElementNS");
-    override(document, "getElementById");
-    override(document, "getElementsByName");
-    override(document, "getElementsByClassName");
-    override(document, "getElementsByTagName");
-    override(document, "getElementsByTagNameNS");
-
-    var scriptNode = document.getElementById("anticanvasfingerprinting");
+    overrideCanvasProto(HTMLCanvasElement);
+    overrideCanvaRendProto(CanvasRenderingContext2D);
+    overrideDocumentProto(Document);
     scriptNode.parentNode.removeChild(scriptNode);
 }
-
-var script = document.createElement("script");
-script.id = "anticanvasfingerprinting";
+var script = document.createElement('script');
+script.id = getRandomString();
 script.type = "text/javascript";
-var newChild = document.createTextNode('try{(' + main + ')(' + self.options.r + ',' + self.options.g + ',' + self.options.b + ',' + self.options.a + ');} catch (e) {}');
-script.appendChild(newChild);
-var node = (document.documentElement || document.head || document.body);
-if (typeof node[self.options.docId] === 'undefined') {
-    node.insertBefore(script, node.firstChild);
-    node[self.options.docId] = getRandomString();
+if (allowInjection) {
+    var newChild = document.createTextNode('try{(' + main + ')(' + self.options.r + ',' + self.options.g + ',' + self.options.b + ',' + self.options.a + ',"'+script.id+'");} catch (e) {console.error(e);}');
+    script.appendChild(newChild);
+    var node = (document.documentElement || document.head || document.body);
+    if (typeof node[self.options.docId] === 'undefined') {
+        node.insertBefore(script, node.firstChild);
+        node[self.options.docId] = getRandomString();
+    }
 }
-
 function getRandomString() {
     var text = "";
     var charset = "abcdefghijklmnopqrstuvwxyz";
